6d964245df6229e323c34dec6dddc4bc66497fda
Dominik Trochowski <dominik.trochowski@gmail.com>
2024-12-26 13:03:39 +0100

    clang: format all files with clang-format tool
    Signed-off-by: Dominik Trochowski <dominik.trochowski@gmail.com>

--- firmware/src/leds.cpp
+++ firmware/src/leds.cpp
@@ -2,11 +2,12 @@
 #include <ranges>
 
 #include "leds.hpp"
-#include "ws2812.pio.h"
 #include "pico/stdlib.h"
+#include "ws2812.pio.h"
 
-Leds::Leds(uint leds_count, PIO pio, uint pin, float freq) :
-    leds_count(leds_count), leds(leds_count, Led(0,0,0)), pio(pio), w_pin(pin), w_freq(freq), sm(0) {}
+Leds::Leds(uint leds_count, PIO pio, uint pin, float freq)
+: leds_count(leds_count), leds(leds_count, Led(0, 0, 0)), pio(pio), w_pin(pin), w_freq(freq), sm(0) {
+}
 
 void Leds::init() {
     offset = pio_add_program(pio, &ws2812_program);
@@ -19,26 +20,18 @@ void Leds::set_led_color(uint led_id, Color color, bool r) {
     if (led_id >= leds.size()) {
         return;
     }
-    switch(color) {
-        case Red:
-            leds[led_id] = Led(255, 0, 0);
-        break;
-        case Green:
-            leds[led_id] = Led(0, 255, 0);
-        break;
-        case Blue:
-            leds[led_id] = Led(0, 0, 255);
-        break;
-        case None:
-            leds[led_id] = Led(0, 0, 0);
-        break;
+    switch (color) {
+    case Red: leds[led_id] = Led(255, 0, 0); break;
+    case Green: leds[led_id] = Led(0, 255, 0); break;
+    case Blue: leds[led_id] = Led(0, 0, 255); break;
+    case None: leds[led_id] = Led(0, 0, 0); break;
     }
     if (r)
         refresh();
 }
 
 void Leds::set_all_color(Color color, bool r) {
-    for (int id = 0; id < leds.size(); id ++) {
+    for (int id = 0; id < leds.size(); id++) {
         set_led_color(id, color);
     }
     if (r)
@@ -55,20 +48,19 @@ void Leds::disable_all(bool r) {
 
 void Leds::push_led(const Led& led) {
     uint32_t color = (static_cast<uint32_t>(led.red) << 16) |
-                     (static_cast<uint32_t>(led.green) << 8)    |
-                     (static_cast<uint32_t>(led.blue));
+    (static_cast<uint32_t>(led.green) << 8) | (static_cast<uint32_t>(led.blue));
 
-    pio_sm_put_blocking(pio, sm, color << 8u); 
+    pio_sm_put_blocking(pio, sm, color << 8u);
 }
 
 void Leds::refresh() {
-    for (const auto led: std::ranges::reverse_view(leds)) {
+    for (const auto led : std::ranges::reverse_view(leds)) {
         push_led(led);
     }
 }
 
 void Leds::blink(Color color, uint count, float freq) {
-    uint32_t delay = static_cast<uint32_t>(((1/freq)/2)*1000);
+    uint32_t delay = static_cast<uint32_t>(((1 / freq) / 2) * 1000);
     for (int i = 0; i < count; i++) {
         set_all_color(color, true);
         sleep_ms(delay);
@@ -78,7 +70,7 @@ void Leds::blink(Color color, uint count, float freq) {
 }
 
 void Leds::blink(uint led_id, Color color, uint count, float freq) {
-    uint32_t delay = static_cast<uint32_t>(((1/freq)/2)*1000);
+    uint32_t delay = static_cast<uint32_t>(((1 / freq) / 2) * 1000);
     for (int i = 0; i < count; i++) {
         set_led_color(led_id, color, true);
         sleep_ms(delay);
