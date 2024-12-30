3613399d757165ac25548ee0feec5daa1b3c7df9
Dominik Trochowski <dominik.trochowski@gmail.com>
2024-12-31 00:15:25 +0100

    clang-format: modify IndentCaseLabels value to true
    Signed-off-by: Dominik Trochowski <dominik.trochowski@gmail.com>

--- firmware/usb/usb_descriptors.cpp
+++ firmware/usb/usb_descriptors.cpp
@@ -189,33 +189,33 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     size_t chr_count;
 
     switch (index) {
-    case STRID_LANGID:
-        memcpy(&_desc_str[1], string_desc_arr[0], 2);
-        chr_count = 1;
-        break;
+        case STRID_LANGID:
+            memcpy(&_desc_str[1], string_desc_arr[0], 2);
+            chr_count = 1;
+            break;
 
-    case STRID_SERIAL: chr_count = board_usb_get_serial(_desc_str + 1, 32); break;
+        case STRID_SERIAL: chr_count = board_usb_get_serial(_desc_str + 1, 32); break;
 
-    default:
-        // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-        // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+        default:
+            // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+            // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
 
-        if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
-            return NULL;
+            if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
+                return NULL;
 
-        const char* str = string_desc_arr[index];
+            const char* str = string_desc_arr[index];
 
-        // Cap at max char
-        chr_count              = strlen(str);
-        size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1; // -1 for string type
-        if (chr_count > max_count)
-            chr_count = max_count;
+            // Cap at max char
+            chr_count = strlen(str);
+            size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1; // -1 for string type
+            if (chr_count > max_count)
+                chr_count = max_count;
 
-        // Convert ASCII string into UTF-16
-        for (size_t i = 0; i < chr_count; i++) {
-            _desc_str[1 + i] = str[i];
-        }
-        break;
+            // Convert ASCII string into UTF-16
+            for (size_t i = 0; i < chr_count; i++) {
+                _desc_str[1 + i] = str[i];
+            }
+            break;
     }
 
     // first byte is length (including header), second byte is string type
