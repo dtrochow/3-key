85d63887c3bd0be8c03e847ceac0d4518e74e94d
Dominik Trochowski <dominik.trochowski@gmail.com>
2025-02-02 20:31:46 +0100

    unit_tests: clean-up unit tests build system
    The unit tests can be added to main CMakeLists file
    much easier. The whole unit test is being declared in
    separate CMakeLists file next to test source and
    then only the test directory must be added to main tests
    CMakeLists file.
    
    Signed-off-by: Dominik Trochowski <dominik.trochowski@gmail.com>

--- firmware/time/test/time_test.cpp
+++ firmware/time/test/time_test.cpp
@@ -22,10 +22,9 @@
 #include "time.hpp"
 #include <gtest/gtest.h>
 
-// Test fixture for Time class
 class TimeTest : public ::testing::Test {
   protected:
-    Time time; // Time object for testing
+    Time time;
 };
 
 TEST_F(TimeTest, DefaultInitialization) {
@@ -35,7 +34,7 @@ TEST_F(TimeTest, DefaultInitialization) {
 }
 
 TEST_F(TimeTest, SetCurrentTime) {
-    uint64_t test_time_us = 1234567890123;
+    const uint64_t test_time_us = 1234567890123;
     time.set_current_time_us(test_time_us);
     EXPECT_EQ(time.get_current_time_us(), test_time_us);
     EXPECT_EQ(time.get_current_time_ms(), test_time_us / 1000);
@@ -43,8 +42,8 @@ TEST_F(TimeTest, SetCurrentTime) {
 }
 
 TEST_F(TimeTest, GetCurrentDateAndTime) {
-    // Set time to 1st January 2022, 00:00:00 (seconds since epoch: 1640995200)
-    uint64_t test_time_us = 1640995200ULL * 1000000;
+    /* Set time to 1st January 2022, 00:00:00 (seconds since epoch: 1640995200) */
+    const uint64_t test_time_us = 1640995200ULL * 1000000;
     time.set_current_time_us(test_time_us);
 
     DateTime_t dt = time.get_current_date_and_time();
@@ -57,8 +56,8 @@ TEST_F(TimeTest, GetCurrentDateAndTime) {
 }
 
 TEST_F(TimeTest, GetCurrentDateAndTimeString) {
-    // Set time to 31st December 2021, 23:59:59
-    uint64_t test_time_us = 1640995199ULL * 1000000;
+    /* Set time to 31st December 2021, 23:59:59 */
+    const uint64_t test_time_us = 1640995199ULL * 1000000;
     time.set_current_time_us(test_time_us);
 
     std::string expected = "31.12.2021 23:59:59";
@@ -66,8 +65,8 @@ TEST_F(TimeTest, GetCurrentDateAndTimeString) {
 }
 
 TEST_F(TimeTest, LeapYearHandling) {
-    // Set time to 29th February 2020, 12:00:00 (a leap year)
-    uint64_t test_time_us = 1582977600ULL * 1000000;
+    /* Set time to 29th February 2020, 12:00:00 (a leap year) */
+    const uint64_t test_time_us = 1582977600ULL * 1000000;
     time.set_current_time_us(test_time_us);
 
     DateTime_t dt = time.get_current_date_and_time();
